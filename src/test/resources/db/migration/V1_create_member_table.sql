4c3ef9e17cee76ada91a8b1f2c6feaf23e040e4b
Oyindamola Subair <osubair@chanzuckerberg.com>
2020-06-11 13:36:17 +0100

    change to secrets

--- src/test/resources/db/migration/V1_create_member_table.sql
+++ src/test/resources/db/migration/V1_create_member_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS recipes.user_by_id (
-    user_id varchar(150),
+    user_id varchar(150) not null,
     username varchar(100) not null,
     email varchar(150) not null,
     password varchar(150) not null,
@@ -7,4 +7,4 @@ CREATE TABLE IF NOT EXISTS recipes.user_by_id (
     timestamp_updated timestamp,
     UNIQUE (user_id, username, email),
     PRIMARY KEY (user_id)
-)   ENGINE=INNODB;
+);
